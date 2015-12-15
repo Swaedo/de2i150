6ea4a78d15433060a49d56e6fcf2593a7811d252
Tiger Cheng <Swaedo@users.noreply.github.com>
2015-12-15 18:56:49 -0500

    why segfault?

--- demo_master_slave/linux_app_sample/app.c
+++ demo_master_slave/linux_app_sample/app.c
@@ -22,13 +22,25 @@ PCIE_BAR pcie_bars[] = { PCIE_BAR0, PCIE_BAR1 , PCIE_BAR2 , PCIE_BAR3 , PCIE_BAR
 void test32( PCIE_HANDLE hPCIe, DWORD addr );
 void testDMA( PCIE_HANDLE hPCIe, DWORD addr);
 
-BOOL WriteImage(PCIE_HANDLE hPCIe, char *filename);
-BOOL checkImageDone(PCIE_HANDLE hPCIe);
-BOOL NewReadImage(PCIE_HANDLE hPCIe);
-void Demo(PCIE_HANDLE hPCIe, char *filename);
-BOOL WriteInfo2(PCIE_HANDLE hPCIe);
+// BOOL WriteImage(PCIE_HANDLE hPCIe, char *filename);
+// BOOL checkImageDone(PCIE_HANDLE hPCIe);
+// BOOL NewReadImage(PCIE_HANDLE hPCIe);
+// void Demo(PCIE_HANDLE hPCIe, char *filename);
+// BOOL WriteInfo2(PCIE_HANDLE hPCIe);
 
-int main(void)
+/*
+demo
+
+write image 
+    write header
+    save image to sdram
+send start signal 
+read image
+    save image to out
+    Write Info
+    */
+
+int main(int arc, char* argv[])
 {
 	void *lib_handle;
 	PCIE_HANDLE hPCIe;
@@ -40,13 +52,67 @@ int main(void)
 		return 0;
 	}
 	hPCIe = PCIE_Open(0,0,0);		// Every device is a like a file in UNIX. Opens the PCIE device for reading/writing
-
+    /*PCIE_Write32( hPCIe, pcie_bars[0], addr, *((unsigned char *)&tempw + 1) ) ;  //01
+    PCIE_Write32( hPCIe, pcie_bars[0], addr, *((unsigned char *)&tempw + 1) ) ;  //01
+    PCIE_Write32( hPCIe, pcie_bars[0], addr, *((unsigned char *)&tempw + 1) ) ;  //01
+    PCIE_Write32( hPCIe, pcie_bars[0], addr, *((unsigned char *)&tempw + 1) ) ;  //01 */
+    
 	if (!hPCIe)
 	{
 		printf("PCIE_Open failed\n");
 		return 0;
 	}
-/*
+    
+  // Custom Logic
+    unsigned char magicNumb[2];  
+    unsigned char width; 
+    unsigned char height;
+    unsigned char maxInt;
+    unsigned long* Image = malloc(width * height * sizeof(unsigned char));
+  
+    //open image
+    printf("a");
+    if (argv[1] == NULL){
+        printf("include file name\n"); 
+        return 1;
+    }
+    printf("b");
+    FILE * pFile = fopen(argv[1], "rb");
+    
+    //strip image
+        printf("1");
+        fread(magicNumb, sizeof(unsigned char), 2, pFile);
+        printf("2");
+        fread(width, sizeof(unsigned char), 1, pFile);
+        printf("3");
+        fread(height, sizeof(unsigned char), 1, pFile);
+        printf("4");
+        fread(maxInt, sizeof(unsigned char), 1, pFile);
+        printf("5");
+        printf("%s %c %c %c\n", magicNumb, width, height, maxInt);
+        printf("6");
+        fread(Image, sizeof(unsigned short), (width*height), pFile);
+        printf("7");
+        
+        DWORD addr = 0x04; //hope this is right for PPM
+        //get width (0008/32)
+        //send width
+        //get height 
+        //send height 
+        //max intens = 255
+        //loop save image array (whats the terminiating condition? = height * width * thing) (0008/32)
+        
+    
+    BOOL bPass;
+    //bPass = PCIE_DmaRead(hPCIe, addr, readArray, MAXDMA * RWSIZE );
+
+    //save image to atom 
+            //open image
+            //p6, width, height, intensity
+            //read remainder from SDRAM
+            
+        
+    /*
 	//test CRA
 	test32(hPCIe, CRA);			// Test the Configuration Registers for reads and writes
 	PCIE_Write32( hPCIe, pcie_bars[0], CRA, START_BYTE);
@@ -56,8 +122,10 @@ int main(void)
 	PCIE_Write32( hPCIe, pcie_bars[0], CRA, STOP_BYTE);
 	printf("\nPush up SW[16] to view data stored in SDRAM and use SW[3:0] to select different addresses.\n");
 */  
-    //test Custom Logic
-   
     
+    
+    fclose(pFile);
+    free(Image);
+    printf("done\n");
 	return 0;
 }
