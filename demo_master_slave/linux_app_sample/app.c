aa1345d2f56d2322c43855dc905828796702ca21
Tiger Cheng <Swaedo@users.noreply.github.com>
2015-12-15 14:19:10 -0500

    template

--- demo_master_slave/linux_app_sample/app.c
+++ demo_master_slave/linux_app_sample/app.c
@@ -19,8 +19,40 @@
 #define RWSIZE (32 / 8)
 PCIE_BAR pcie_bars[] = { PCIE_BAR0, PCIE_BAR1 , PCIE_BAR2 , PCIE_BAR3 , PCIE_BAR4 , PCIE_BAR5 };
 
+typedef struct tagBITMAPFILEHEADER
+{
+    WORD bfType;  //specifies the file type
+    DWORD bfSize;  //specifies the size in bytes of the bitmap file
+    WORD bfReserved1;  //reserved; must be 0
+    WORD bfReserved2;  //reserved; must be 0
+    DWORD bOffBits;  //species the offset in bytes from the bitmapfileheader to the bitmap bits
+}BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER
+{
+    
+    /*signed int*/ WORD  width;  //specifies width in pixels
+    /*signed int*/ WORD dummy2;  //species height in pixels
+    WORD height;
+    DWORD dummy3;
+    WORD biPlanes; //specifies the number of color planes, must be 1
+    WORD biBitCount; //specifies the number of bit per pixel
+    DWORD biCompression;//spcifies the type of compression
+    DWORD biSizeImage;  //size of image in bytes
+    signed int biXPelsPerMeter;  //number of pixels per meter in x axis
+    signed int biYPelsPerMeter;  //number of pixels per meter in y axis
+    DWORD biClrUsed;  //number of colors used by th ebitmap
+    DWORD biClrImportant;  //number of colors that are important
+}BITMAPINFOHEADER;
+
 void test32( PCIE_HANDLE hPCIe, DWORD addr );
 void testDMA( PCIE_HANDLE hPCIe, DWORD addr);
+
+BOOL WriteImage(PCIE_HANDLE hPCIe, char *filename, BITMAPINFOHEADER *info);
+BOOL checkImageDone(PCIE_HANDLE hPCIe);
+BOOL NewReadImage(PCIE_HANDLE hPCIe, BITMAPINFOHEADER *info);
+void Demo(PCIE_HANDLE hPCIe, char *filename);
+BOOL WriteInfo2(PCIE_HANDLE hPCIe, BITMAPINFOHEADER *info);
 
 int main(void)
 {
@@ -40,7 +72,7 @@ int main(void)
 		printf("PCIE_Open failed\n");
 		return 0;
 	}
-
+/*
 	//test CRA
 	test32(hPCIe, CRA);			// Test the Configuration Registers for reads and writes
 	PCIE_Write32( hPCIe, pcie_bars[0], CRA, START_BYTE);
@@ -49,9 +81,14 @@ int main(void)
 
 	PCIE_Write32( hPCIe, pcie_bars[0], CRA, STOP_BYTE);
 	printf("\nPush up SW[16] to view data stored in SDRAM and use SW[3:0] to select different addresses.\n");
+*/  
+    //test Custom Logic
+   
+    
 	return 0;
 }
 
+/*
 //Tests 16 consecutive PCIE_Write32 to address
 
 void test32( PCIE_HANDLE hPCIe, DWORD addr )
@@ -135,3 +172,104 @@ void testDMA( PCIE_HANDLE hPCIe, DWORD addr)
 	}
 	return;
 }
+*/
+BOOL NewReadImage(PCIE_HANDLE hPCIe, BITMAPINFOHEADER *info) 
+{
+	//checkImageDone(hPCIe);
+	FILE* fp = fopen("out.bmp", "wb");
+
+    static unsigned char //color[1], /* 1 byte = 8 bits */
+                         byte[1];
+    unsigned short word[1]; /* 2 bytes */
+    unsigned long  dword[1], /* 4 bytes */
+                   bpp=1,
+                   /* in bytes */
+                   FileHeaderSize=14, 
+                   InfoHeaderSize=40, // header_bytes
+                   PaletteSize=255*4, // = 8 = number of bytes in palette, 256 colors
+                   BytesPerRow,
+                   FileSize,
+                   OffBits,
+                   BytesSize, // bytes in image portion
+                   //iByte, // number of byte
+                   /* in pixels */
+                   Width= info->width, 
+                   Height= info->height;
+                   /* in ... */
+                   //ix, iy, ix_,
+                   //bit=0, /* bit value */
+                   //bitNumber; /* bits are numberd from 0 to 7 */
+
+    BytesPerRow=(((Width * bpp)+31)/32)*4; 
+    BytesSize=BytesPerRow*Height;
+    FileSize=FileHeaderSize+InfoHeaderSize+PaletteSize+BytesSize;
+    OffBits=FileHeaderSize+InfoHeaderSize+PaletteSize;
+
+    word[0]=19778;                                         fwrite(word,1,2,fp); /* file Type signature = BM */
+    dword[0]=FileSize;                                     fwrite(dword,1,4,fp); /* FileSize */
+    word[0]=0;                                             fwrite(word,1,2,fp); /* reserved1 */
+    word[0]=0;                                             fwrite(word,1,2,fp); /* reserved2 */
+    dword[0]=OffBits;                                      fwrite(dword,1,4,fp); /* OffBits */
+    dword[0]=InfoHeaderSize;                               fwrite(dword,1,4,fp); 
+    dword[0]=Width;                                        fwrite(dword,1,4,fp); 
+    dword[0]=Height;                                       fwrite(dword,1,4,fp); 
+    word[0]=1;                                             fwrite(word,1,2,fp); /* planes */
+    word[0]=8;                                             fwrite(word,1,2,fp); /* Bits of color per pixel */
+    dword[0]=0;                                            fwrite(dword,1,4,fp); /* compression type */
+    dword[0]=0;                                            fwrite(dword,1,4,fp); /* Image Data Size, set to 0 when no compression */
+    dword[0]=0;                                            fwrite(dword,1,4,fp); /*  */
+    dword[0]=0;                                            fwrite(dword,1,4,fp); /*  */
+    dword[0]=255;                                          fwrite(dword,1,4,fp); /*  number of used coloors*/
+    dword[0]=0;                                            fwrite(dword,1,4,fp); /*  */
+ 
+    int i;
+    for (i = 0; i < 256; i++)
+    {
+    	byte[0]=i;                                            fwrite(byte,1,1,fp); /* R */ 
+		byte[0]=i;                                            fwrite(byte,1,1,fp); /* R */                                     
+		byte[0]=i;                                            fwrite(byte,1,1,fp); /* G */    
+		byte[0]=i;                                            fwrite(byte,1,1,fp); /* B */ 
+    }          
+
+    DWORD addr = 0x08500000; //new image starts from 0x08500000
+
+	unsigned char *testImage;//test image buffer
+	testImage = (unsigned char*)malloc(info->width*info->height*4*sizeof(unsigned char));
+	BOOL bPass = PCIE_DmaRead(hPCIe, addr, testImage, info->width*info->height*4);
+	if(!bPass)
+	{
+		printf("ERROR: unsuccessful image reading.\n");
+		return FALSE;
+	}
+	else
+		printf("Image read by the Atom.\n");
+
+
+	unsigned char *bitmapImage;//image buffer
+	bitmapImage = (unsigned char*)malloc(info->width*info->height*sizeof(unsigned char));
+	int index = 0;
+	int j;
+	// Store only the LSByte of the 32-bit data into the new image file
+	for (j = 0; j < info->width*info->height; ++j)
+	{
+		unsigned char buff;
+		buff = testImage[index++];
+		bitmapImage[j] = buff;
+		buff = testImage[index++];
+		buff = testImage[index++];
+		buff = testImage[index++];
+	}
+
+	if(!bitmapImage)
+	{
+		printf("ERROR: new image is empty.\n");
+		return FALSE;
+	}
+	fwrite(bitmapImage, info->width*info->height*sizeof(unsigned char),1,fp);
+	free(bitmapImage);
+	free(testImage);
+	fclose(fp);
+	printf("New image written to **out.bmp**.\n");
+	return TRUE;
+}
+
